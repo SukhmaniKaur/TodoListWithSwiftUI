836e59102e362042dad459cb0055febe6db37f96
Sukhmani Kaur <sukhmani.ghataura@byjus.com>
2022-11-28 14:46:57 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sukhmani Kaur on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
