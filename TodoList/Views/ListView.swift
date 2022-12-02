454a6643840b638f9b9c3647c18f293b827b1ee2
Sukhmani Kaur <sukhmani.ghataura@byjus.com>
2022-12-02 20:30:34 +0530

    designed the main view of the app List View

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ListView: View {
+    
+    @State var items: [String] = [
+    "This is the first item!",
+    "This is the second item!",
+    "This is the third item!"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Todo List 📝")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing: NavigationLink("Add", destination: {
+                AddView()
+            }))
     }
 }
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        NavigationView {
+            ListView()
+        }
     }
 }
+
+
