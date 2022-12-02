7ecca5c62b00d077c24b182469b71a71fba2f7bf
Sukhmani Kaur <sukhmani.ghataura@byjus.com>
2022-12-02 20:31:17 +0530

    designed the add todo item view

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @State var textfieldString: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textfieldString)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                .background(Color.gray).opacity(0.45)
+                .cornerRadius(10)
+                Button {
+                    // todo
+                } label: {
+                    Text("SAVE")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖊")
     }
 }
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        AddView()
+        NavigationView {
+            AddView()
+        }
     }
 }
