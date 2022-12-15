7d14818d93f7c88fe837a2993705f9c116206b03
Sukhmani Kaur <sukhmani.ghataura@byjus.com>
2022-12-15 20:16:54 +0530

    saved and persisted the items list data with user defaults.

--- TodoList/ViewModel/ListViewModel.swift
+++ TodoList/ViewModel/ListViewModel.swift
@@ -9,19 +9,26 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
-    func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first item!", isCompleted: false),
-            ItemModel(title: "This is the second item!", isCompleted: true),
-            ItemModel(title: "This is the third item!", isCompleted: false)
-        ]
-        items.append(contentsOf: newItems)
+    // getItems
+    func getItems() {        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     //MARK: - CRUD FUNCTIONS
@@ -41,6 +48,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id })  {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
